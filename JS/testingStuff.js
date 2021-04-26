f1230c52ec4b32fcfc71a936f6e427c39b57b767
sergiogyoz <sergiogyoz@hotmail.com>
2021-04-26 02:15:12 -0500

    Header Layout fixes

--- JS/testingStuff.js
+++ JS/testingStuff.js
@@ -1,4 +1,17 @@
 
 
 
+function changetitles(title) {
+    var mainTitle=document.getElementById("main-title");
+    var headerTitle=document.getElementsByTagName("h1");
+    var titulo="Paola Lafourie";   
+    mainTitle.textContent=titulo;
+    headerTitle[0].textContent=titulo;
+}
+
+window.onload=function () {
+    changetitles("");
+}
+
+
 
