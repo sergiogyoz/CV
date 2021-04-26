aa2b0d4c439ff7eb1e1ed8da30b2557b1d7e4f11
sergiogyoz <sergiogyoz@hotmail.com>
2021-04-26 02:26:05 -0500

    last header fixes
    forgot about these

--- JS/testingStuff.js
+++ JS/testingStuff.js
@@ -4,7 +4,7 @@
 function changetitles(title) {
     var mainTitle=document.getElementById("main-title");
     var headerTitle=document.getElementsByTagName("h1");
-    var titulo="Paola Lafourie";   
+    var titulo="Sergio Villamarin";   
     mainTitle.textContent=titulo;
     headerTitle[0].textContent=titulo;
 }
