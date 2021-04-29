f72aebd5b3e1160b056872dac96ff98bb54cf27a
sergiogyoz <sergiogyoz@hotmail.com>
2021-04-29 00:09:21 -0500

    Testing classes and tree traversing
    Made a simple section class which I later used in the "addSection" function that creates and fills a section with the Section object.
    
    Basically, me learning classes and some navigation/creation DOM methods

--- JS/testingStuff.js
+++ JS/testingStuff.js
@@ -1,9 +1,8 @@
 function changetitles(title) {
     var mainTitle=document.getElementById("main-title");
-    var headerTitle=document.getElementsByTagName("h1");
-    var titulo="Sergio Villamarin";   
-    mainTitle.textContent=titulo;
-    headerTitle[0].textContent=titulo;
+    var headerTitle=document.getElementsByTagName("h1");  
+    mainTitle.textContent=title;
+    headerTitle[0].textContent=title;
 }
 
 function navbarbehavior(){
@@ -11,13 +10,43 @@ function navbarbehavior(){
     mainTitle.classList.add("main-title-fixed");
     /*mainTitle.style.position="fixed";
     mainTitle.style.bottom="initial";
-    mainTitle.style.top="0px";*/
-    alert(mainTitle.className);
-    
+    mainTitle.style.top="0px";
+    alert(mainTitle.className);*/
 }
 
+class Section{
+    constructor(title, content){this.title=title;   this.content=content;}
+    
+    toString() {
+        return (this.title+":  \n"+this.content);
+    }
+}
+
+function addSection(Section) {
+    var sections=document.getElementById("sections");
+    console.log(sections.className);
+    var snode= document.createElement("div");
+    snode.classList.add("section");
+    snode.id="crazy-section";
+    var t=document.createElement("h2");
+    t.append(document.createTextNode(Section.title));
+    var c=document.createElement("p");
+    c.append(document.createTextNode(Section.content));
+    snode.append(t);
+    snode.append(c);
+    sections.append(snode);
+}
+
+
 window.onload=function () {
-    changetitles("");
+    changetitles("Sergio Villamarin");
     navbarbehavior();
+    /*testing objects*/
+    let x=new Section("mis cositas","Aqui va una detallada explicacion de mis cositas");
+    /*testing tree transversing*/
+    addSection(x);
+    addSection( new Section("La chutengcia","Cosas geomorfologas de GIS, el Lidar del geodolito nos permite hacer un paralaje inesperado y scientifico") );
+
+
 }
 
