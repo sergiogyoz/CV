c00ea48ededc2974364c1ba09abe77998ea8658c
sergiogyoz <sergiogyoz@hotmail.com>
2021-07-09 13:57:57 -0500

    more main page layout: footer area
    Almost there, I think I just need the secondary menu for the mobile version and we can probably focus on the non-main pages and then finally add content

--- JS/main.js
+++ JS/main.js
@@ -4,14 +4,13 @@ const nav=document.getElementsByTagName("nav")[0];
 let lineon=false;
 window.addEventListener("scroll", function(){
     if( !lineon && window.pageYOffset>150){
-
         nav.classList.add("transition");
         lineon= true;
-        console.log(window.pageYOffset+" put on");
     }
     if( lineon && window.pageYOffset<150){
         nav.classList.remove("transition");
         lineon= false;
-        console.log(window.pageYOffset+" take off");
     }
 })
+
+//
